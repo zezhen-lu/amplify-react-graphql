7c2ca47e23e5283309584a75ca0e891769ecf0a9
luzezhen <772035658@qq.com>
2023-03-01 17:24:27 +0900

    add

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Add />
   </React.StrictMode>
 );
 
